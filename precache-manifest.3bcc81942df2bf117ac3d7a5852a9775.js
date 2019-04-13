ffca8b036b8d69bb936afae34fe35e107c083f36
Cait Sidener <cait.sidener@gmail.com>
2019-04-13 10:45:08 -0500

    Updates

---
+++ precache-manifest.3bcc81942df2bf117ac3d7a5852a9775.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "fa7780a2ba9945f17904",
+    "url": "/clicky-game/static/js/main.b42a9400.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "fa7780a2ba9945f17904",
+    "url": "/clicky-game/static/css/main.0599a3e0.chunk.css"
+  },
+  {
+    "revision": "744320d5a349ebcc28553304a20d3294",
+    "url": "/clicky-game/index.html"
+  }
+];
